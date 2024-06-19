6bd2d6804ba03ce63a14fb7bcb61a4941e4c10cd
Администратор <@DESKTOP-IIEASM6>
2024-06-19 21:31:37 +0300

    Add project files.

---
+++ Inheritance/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+class Human
+{
+    string last_name;
+    string first_name;
+    unsigned int age;
+public:
+    void set_age(unsigned int age)
+    {
+        this->age = age;
+    }
+};
+
+class Student : Human
+{
+
+};
+
+int main()
+{
+    Student Oleg;
+    Oleg.set_age(22);
+}
