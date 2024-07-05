1fe8544cfc18c90d23d02ded00dc3e9d05f3a46c
Agash_hk16@top-academy.ru <Agash_hk16@top-academy.ru>
2024-07-05 17:35:38 +0300

    Homework in process

--- AbstractBaseClass/main.cpp
+++ AbstractBaseClass/main.cpp
@@ -1,35 +1,168 @@
+#define _USE_MATH_DEFINES
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-class Animal
+enum tFigure
 {
-public:
-	virtual void sound() = 0;
+	tCircle = 0,
+	tSquare = 1,
+	tTriangle = 2,
+	tRectangle = 3
 };
-class Cat : public Animal {};
-class Tiger : public Cat
+
+class Figure
 {
 public:
-	void sound() override
+	virtual void print()const = 0;
+	virtual void draw()const = 0;
+	//virtual void initialize() = 0;
+	//virtual Figure initialize
+};
+class Circle : public Figure
+{
+	int radius;
+public:
+	Circle(int radius = 0)
 	{
-		cout << "RRRRrrrr" << endl;
+		this->radius = radius;
+		cout << "DefaultConstructor\t" << this << endl;
+	}
+	//void initialize(int radius = rand() % 10) override
+	void initialize(int radius = rand() % 10)
+	{
+		this->radius = radius;
+	}
+	void print()const override
+	{
+		cout << "Радиус круга: " << radius << endl;
+		cout << "Периметр: " << 2 * M_PI * radius << endl;
+		cout << "Площадь: " << M_PI * radius * radius << endl;
+	}
+	void draw()const override
+	{
+
 	}
 };
-class HomeCat : public Cat
+class Square : public Figure
 {
+	int a;
 public:
-	void sound() override
+	Square(int a = 0)
 	{
-		cout << "Meow" << endl;
+		this->a = a;
+		cout << "DefaultConstructor\t" << this << endl;
+	}
+	void initialize(int a = rand() % 10)
+	{
+		this->a = a;
+	}
+	void print()const override
+	{
+		cout << "Длина стороны квадрата: " << a << endl;
+		cout << "Периметр: " << a * 4 << endl;
+		cout << "Площадь: " << a * a << endl;
+	}
+	void draw()const override
+	{
+
 	}
 };
+class Triangle : public Figure
+{
+	int a, b, c;
+public:
+	Triangle(int a = 0, int b = 0, int c = 0)
+	{
+		this->a = this->b = this->c = 0;
+		cout << "DefaultConstructor\t" << this << endl;
+	}
+	void initialize(int a = rand() % 100, int b = rand() % 100, int c = rand() % 100)
+	{
+		this->a = a;
+		this->b = b;
+		this->c = c;
+	}
+	void print()const override
+	{
+		cout << "Длина сторон треугольника:\na = " << a << "\tb = " << b << "\tc = " << c << endl;
+		cout << "Периметр: " << a + b + c << endl;
+		cout << "Площадь: " << sqrt(sp() * (sp() - a) * (sp() - b) * (sp() - c)) << endl;
+	}
+	void draw()const override
+	{
+
+	}
+	double sp()const //semiperimeter
+	{
+		return (a + b + c) / 2;
+	}
+};
+class Rectangle : public Figure //: public Square
+{
+	int a;
+	int b;
+public:
+	Rectangle(int a = 0, int b = 0)
+	{
+		this->a = a;
+		this->b = b;
+		cout << "DefaultConstructor\t" << this << endl;
+	}
+	void initialize(int a = rand() % 10, int b = rand() % 10)
+	{
+		this->a = a;
+		this->b = b;
+	}
+	void print()const override
+	{
+		cout << "Длина сторон прямоугольника:\na = " << a << "\tb = " << b << endl;
+		cout << "Периметр: " << a * 2 + b * 2 << endl;
+		cout << "Площадь: " << a * b << endl;
+	}
+	void draw()const override
+	{
+
+	}
+};
+
 int main()
 {
 	setlocale(LC_ALL, "");
-	//Animal animal; //object of abstract class type is not allowed
-	//Cat cat;
-	Tiger tiger;
-	tiger.sound();
-	HomeCat homecat;
-	homecat.sound();
+	srand(time(NULL));
+	for (int i = 0; i < 5; i++)
+	{
+
+		switch (rand() % 5) 
+		{
+		case tCircle:
+			{
+				Circle temp; //initialization of 'temp' is skipped by 'case' label
+				temp.initialize();
+				temp.print();
+				break;
+			}
+		case tSquare:
+			{
+				Square temp;
+				temp.initialize();
+				temp.print();
+				break;
+			}
+		case tTriangle:
+			{
+				Triangle temp;
+				temp.initialize();
+				temp.print();
+				break;
+			}
+		case tRectangle:
+			{
+				Rectangle temp;
+				temp.initialize();
+				temp.print();
+				break;
+			}
+		}
+	}
 }
